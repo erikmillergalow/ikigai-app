44b5c6da14b51b5376617fc6e7851eb7a7fd27a2
Erik Jurgen Miller-Galow <erikmillergalow@gmail.com>
2019-07-20 00:11:02 -0400

    Initial Commit

---
+++ ikigai-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ikigai-app
+//
+//  Created by Erik J Miller-Galow on 7/20/19.
+//  Copyright © 2019 Erik Jurgen Miller-Galow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ikigai-appTests/ikigai_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  ikigai_appTests.swift
+//  ikigai-appTests
+//
+//  Created by Erik J Miller-Galow on 7/20/19.
+//  Copyright © 2019 Erik Jurgen Miller-Galow. All rights reserved.
+//
+
+import XCTest
+@testable import ikigai_app
+
+class ikigai_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
