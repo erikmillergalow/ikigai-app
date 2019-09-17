e04c7c0fb79e9e45354fe85bc423fae42b4448cb
Erik Jurgen Miller-Galow <erikmillergalow@gmail.com>
2019-09-17 00:43:30 -0400

    Data entry view takes date from the calendar view

--- ikigai-app/CalendarViewController.swift
+++ ikigai-app/CalendarViewController.swift
@@ -10,21 +10,28 @@ import UIKit
 
 class CalendarViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    // send the selected date to the next screen
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if (segue.identifier == "calendarToDataEntry") {
+            if let dataEntryView = segue.destination as? DataEntryViewController {
+                
+                // get date string from the datepicker
+                let dateFormatter = DateFormatter()
+                dateFormatter.timeStyle = .none
+                dateFormatter.dateStyle = .medium
+                let dateString = dateFormatter.string(from: self.datePicker.date)
+                
+                // send the date to the next view
+                dataEntryView.dateString = dateString
+            }
+        }
     }
-    */
 
 }
